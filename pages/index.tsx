6caaa6cc88256109646e1ef05f44f2ccca5047ab
Riku Rouvila <riku.rouvila@gmail.com>
2020-08-29 11:54:37 +0300

    add prettier

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,16 @@
-import Head from "next/head";
+import Head from 'next/head';
 import SpreadSheet, {
   CellConfig,
   defaultSheets,
   FunctionArgument,
   Sheet,
   format as defaultFormatter,
-} from "@rowsncolumns/spreadsheet";
-import { useEffect, useState } from "react";
-import lzString from "lz-string";
-import jsonpack from "jsonpack";
+} from '@rowsncolumns/spreadsheet';
+import { useEffect, useState } from 'react';
+import lzString from 'lz-string';
+import jsonpack from 'jsonpack';
 
-import { parse, interpret } from "../lib/lisp";
+import { parse, interpret } from '../lib/lisp';
 
 function encodeCells(cells: Record<string, Record<string, CellConfig>>) {
   return lzString.compressToEncodedURIComponent(jsonpack.pack(cells));
@@ -24,34 +24,27 @@ function decodeCells(cellsString: string) {
 
 function solveDependencies(forms, sheet) {
   return forms.map((value) => {
-    if(Array.isArray(value)) {
-      return solveDependencies(value, sheet)
+    if (Array.isArray(value)) {
+      return solveDependencies(value, sheet);
     }
 
-    if(value.type === "identifier" && /[A-Z]\d/.test(value.value)) {
-      const [column, row] = value.value.split('')
-      const cell = sheet.cells[row][column.charCodeAt(0) - 64]
-      if(cell.datatype === 'number') {
-        return {type: 'number', value: Number(cell.text)}
+    if (value.type === 'identifier' && /[A-Z]\d/.test(value.value)) {
+      const [column, row] = value.value.split('');
+      const cell = sheet.cells[row][column.charCodeAt(0) - 64];
+      if (cell.datatype === 'number') {
+        return { type: 'number', value: Number(cell.text) };
       }
 
-
-      return {type: 'identifier', value: cell.text}
+      return { type: 'identifier', value: cell.text };
     }
-    return value
-  })
+    return value;
+  });
 }
 
 const formatter = (value, datatype, cellConfig, sheet: Sheet) => {
-
-
-  if (value[0] === "=") {
-
-
+  if (value[0] === '=') {
     try {
-
-
-      const forms = solveDependencies(parse(value.substr(1)), sheet)
+      const forms = solveDependencies(parse(value.substr(1)), sheet);
 
       console.log(interpret(forms));
 
@@ -59,12 +52,8 @@ const formatter = (value, datatype, cellConfig, sheet: Sheet) => {
     } catch (error) {
       console.log(error);
 
-      return "ERROR";
+      return 'ERROR';
     }
-
-    // console.log("tää2", parse("(+ 1 2 3)"));
-    // console.log(closer.parse(value.substr(1)));
-    // return escodegen.generate(closer.parse(value.substr(1)));
   }
 
   return defaultFormatter(value, datatype, cellConfig);
@@ -117,14 +106,14 @@ export default function Home() {
     //   });
     // });
 
-    window.history.pushState(null, null, "#" + encodeCells(sheets[0].cells));
+    window.history.pushState(null, null, '#' + encodeCells(sheets[0].cells));
     setSheets(sheets);
   };
   return (
     <div>
       <Head>
         <title>Sheetpad</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel='icon' href='/favicon.ico' />
       </Head>
 
       <SpreadSheet
@@ -133,7 +122,7 @@ export default function Home() {
         onChange={persistSheet}
         showToolbar={false}
         showTabStrip={false}
-        initialColorMode={"dark"}
+        initialColorMode={'dark'}
         autoFocus={false}
         disableFormula={true}
       />
